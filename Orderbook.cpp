08e8a477ae53c37fb58ff25719ea328c29441b12
Caleb <KlbFms@gmail.com>
2025-02-21 08:13:12 +0000

    Matching function :O

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
 #include <map>
 #include <unordered_map>
 
 
 enum class OrderType {
 
+    MARKET,
     LIMIT,
     CANCEL
 };
@@ -19,7 +21,7 @@ enum class OrderSide {
 
 // fixed sizes cross platform,
 // Quantity and orderID can and should never be negative
-using Price = std::double_t;
+using Price = std::int32_t;
 using Quantity = std::uint64_t;
 using OrderId = std::uint64_t;
 
@@ -37,27 +39,36 @@ public:
     Order(OrderId id, Quantity qty, Price price, OrderType type, OrderSide side) {
         _orderId = id;
         _quantity = qty;
-        _currentQuantity = qty;
-
+        _initialQuantity = qty;
         _price = price;
         _orderSide = side;
         _orderType = type;
     }
 
+    int a { 5 };
+
     // Fill logic for order, cannot reduce past current ammout
     void Fill(Quantity qty) {
-        if (qty > _currentQuantity) {
+        if (qty > _quantity) {
             return;
         }
 
-        _currentQuantity -= qty;
+        _quantity -= qty;
     }
+
+    OrderSide getSide()     { return _orderSide; }
+    OrderType getType()     { return _orderType; }
+    Price getPrice()        { return _price; }
+    Quantity getQuantity()  { return _quantity; }
+    
+
+
 
 private:
     // Price, OrderID, Quantity, OrderSide, OrderType
 
     Quantity _quantity;
-    Quantity _currentQuantity;
+    Quantity _initialQuantity;
     OrderId _orderId;
     Price _price;
     OrderSide _orderSide;
@@ -65,6 +76,9 @@ private:
 
 
 };
+
+
+using OrderPointer = std::shared_ptr<Order>;
 
 
 /*
@@ -76,11 +90,106 @@ private:
 */
 
 class Orderbook {
+private:
 
-    // Orders grouped by price --  15$ : 
-    std::map<
+    // Orders grouped by price --  15$ :  [ OrderId, Quantity etc...]
+    std::map<Price, std::vector<Order>, std::greater<Price>> bids;
+    std::map<Price, std::vector<Order>> asks;
+
+    std::unordered_map<OrderId, Order> orderMap;
+
+public:
+
+    bool CanMatch() {
 
 
+
+    }
+
+    // Get best of both books, match if possible
+    void MatchOrders() {
+
+        if (!CanMatch()) { return; }
+
+
+        while (true) {
+
+            if (bids.empty() || asks.empty()) break;
+
+            auto bidIt = bids.begin();
+            auto askIt = asks.begin();
+
+            Price bidPrice = bidIt->first;
+            Price askPrice = askIt->first;
+
+            while (bids.size() && asks.size()) {
+
+                if (askPrice < bidPrice) {
+                     
+                    // pointer >> if order is large? (or is naive)
+                    auto bidLevel = bidIt->second;
+                    auto askLevel = askIt->second;
+
+                    Order bidOrder = bidLevel.front();
+                    Order askOrder = askLevel.front();
+
+                    Quantity bidQuantity{ bidOrder.getQuantity() };
+                    Quantity askQuantity{ askOrder.getQuantity() };
+
+                    if (askQuantity > bidQuantity) {
+                        askOrder.Fill(bidQuantity);
+                        bidOrder.Fill(bidQuantity);
+
+                        //bids.
+                    }
+                    else {
+                        askOrder.Fill(askQuantity);
+                        bidOrder.Fill(askQuantity);
+                    }
+
+                }
+            }
+
+            
+
+            
+        }
+    }
+
+
+    // needs to return some data on what was traded (if anything was traded
+    void AddOrder(Quantity qty, Price price, OrderType type, OrderSide side) {
+
+        // scheme for new orderids (prob j increment)
+        // static placeholder
+        OrderId r{ 11234 };
+
+
+
+        if (type == OrderType::MARKET) {
+            price = 0;
+        }
+
+        Order newOrder{ r, qty, price, type, side };
+
+
+        // prob bad practice (read up on pointer logic :)) )
+        // Order* orderpointer{ &newOrder };
+        orderMap[r] = newOrder;
+
+        if (side == OrderSide::BUY) {
+            bids[price].push_back(newOrder);
+
+        }
+        else
+        {
+            asks[price].push_back(newOrder);
+        }
+
+        orderMap.insert({ r, newOrder });
+        MatchOrders();
+
+    }
 };
 
 
@@ -88,5 +197,5 @@ class Orderbook {
 // good start :))
 int main()
 {
-    Order myorder = Order(51, 511, 51, OrderType::LIMIT, OrderSide::BUY);
+    Order myorder { 51, 511, 51, OrderType::LIMIT, OrderSide::BUY };
 }
